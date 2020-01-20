31808422e8804dfa8887cb65f0d7596b46d0d8ee
Atique Ahmed ANMEPL BLR <atique.ahmed@asianetnews.in>
2020-01-20 23:52:56 +0530

    pipeline code

--- Tutorial-8_Jenkins-PipelineScript/pipelinescript.sh
+++ Tutorial-8_Jenkins-PipelineScript/pipelinescript.sh
@@ -14,7 +14,7 @@ pipeline {
 			steps {
 				echo 'Prepare Environment'
 				sshagent(credentials: ['28ebc607-22f5-4fad-91f2-97de971512d3'], ignoreMissing: true) {
-                  sh 'ssh -o StrictHostKeyChecking=no -l ubuntu 13.234.202.100 "cd ./youtube_angular7__weather_forcast_testing; npm install"'
+                  sh 'ssh -o StrictHostKeyChecking=no -l ubuntu 13.234.202.100 "cd ./youtube_angular7__weather_forcast_testing; npm install; pm2 stop app.sh; pm2 delete app.sh"'
                 }
             }
 		}
