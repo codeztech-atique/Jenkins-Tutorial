7ecc254e1e968a0a970d02f318b68a2b53d5a0f3
Atique Ahmed ANMEPL BLR <atique.ahmed@asianetnews.in>
2020-01-21 00:52:52 +0530

    reaggange

--- Youtube-Jenkins-Require-Package/required_package.sh
+++ Youtube-Jenkins-Require-Package/required_package.sh
@@ -2,10 +2,8 @@
 
 bitbucket
 slack notification
-gitHub plugin
-Generic Webhook Trigger
 SSH Agent -- For Jenkins Pipeline, SSH Login
 SSH --- For Jenkins SSH Loging
-Also install build monitor view
+build monitor view
 
 sudo /etc/init.d/jenkins restarts
